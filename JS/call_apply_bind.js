cfaf6de6a085008e7bda5f68a9ae879835e93e31
pamuba <partha.bora24@gmail.com>
2021-06-30 19:30:09 +0530

    91

---
+++ JS/call_apply_bind.js
@@ -0,0 +1,23 @@
+// //1.Call
+
+// var pokemon = {
+//     firstName:'Pika',
+//     lastName:'Chu',
+//     //Function
+//     getPokeName : function(){
+//         var fullName = this.firstName+' '+this.lastName;
+//         return fullName;
+//     }
+// }
+
+// var pokemonName = function(snack, hobby){
+//     // console.log(this)
+//     console.log(this.getPokeName() + ' I choose you')
+//     console.log(this.getPokeName()+' loves '+snack+' and '+hobby)
+// }
+
+// var bindPokemon =  pokemonName.bind(pokemon)
+// //10 lines of code
+// bindPokemon('pizza', 'eating')
+
+//2.call
