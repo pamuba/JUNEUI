fec9a5173d82460ae671dcab88d76b7f9d0116db
pamuba <partha.bora24@gmail.com>
2021-07-20 20:07:13 +0530

    201

--- D3Demo/update/js/main.js
+++ D3Demo/update/js/main.js
@@ -2,6 +2,8 @@ const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
 
+let flag = true;
+
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
@@ -19,34 +21,57 @@ g.append("text")
   .text("Month")
 
 // Y label
-g.append("text")
+  const yLabel = g.append("text")
   .attr("class", "y axis-label")
   .attr("x", - (HEIGHT / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
   .attr("transform", "rotate(-90)")
-  .text("Revenue ($)")
+  
+
+const x = d3.scaleBand()
+  .range([0, WIDTH])
+  .paddingInner(0.3)
+  .paddingOuter(0.2)
+
+const y = d3.scaleLinear()
+  .range([HEIGHT, 0])
+
+const xAxisGroup = g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", `translate(0, ${HEIGHT})`)
+
+const yAxisGroup = g.append("g")
+  .attr("class", "y axis")
+
 
 d3.csv("data/revenues.csv").then(data => {
   data.forEach(d => {
     d.revenue = Number(d.revenue)
+    d.profit = Number(d.profit)
   })
 
-  const x = d3.scaleBand()
-    .domain(data.map(d => d.month))
-    .range([0, WIDTH])
-    .paddingInner(0.3)
-    .paddingOuter(0.2)
-  
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.revenue)])
-    .range([HEIGHT, 0])
+  console.log(data)
+
+  d3.interval(() => {
+    flag = !flag;
+    update(data)
+  }, 1000)
+
+  update(data)
+})
+
+function update(data){
+  const value = flag ? "profit" : "revenue";
+
+  x.domain(data.map(d => d.month))
+  y.domain([0, d3.max(data, d => d[value])])
+
+
 
   const xAxisCall = d3.axisBottom(x)
-  g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0, ${HEIGHT})`)
+  xAxisGroup
     .call(xAxisCall)
     .selectAll("text")
       .attr("y", "10")
@@ -57,21 +82,32 @@ d3.csv("data/revenues.csv").then(data => {
   const yAxisCall = d3.axisLeft(y)
     .ticks(3)
     .tickFormat(d => d + "m")
-  g.append("g")
-    .attr("class", "y axis")
+    yAxisGroup
     .call(yAxisCall)
 
+  //DATA JOIN, join in new elements
   const rects = g.selectAll("rect")
     .data(data)
-  
-  rects.enter().append("rect")
-    .attr("y", d => y(d.revenue))
+
+  //EXIT old elements not present in the new data
+  rects.exit().remove();
+
+  //UPDATE old elements present in the new data
+  rects
+    .attr("y", d => y(d[value]))
     .attr("x", (d) => x(d.month))
     .attr("width", x.bandwidth)
-    .attr("height", d => HEIGHT - y(d.revenue))
+    .attr("height", d => HEIGHT - y(d[value]))
+
+  //ENTER new elements present in the new data
+  rects.enter().append("rect")
+    .attr("y", d => y(d[value]))
+    .attr("x", (d) => x(d.month))
+    .attr("width", x.bandwidth)
+    .attr("height", d => HEIGHT - y(d[value]))
     .attr("fill", "grey")
 
-  d3.interval(() => {
-    console.log("Hello World")
-  }, 1000)
-})
+    const text = flag ? "Profit ($)" : "Revenue ($)";
+    yLabel.text(text);
+    // console.log(rects)
+}
